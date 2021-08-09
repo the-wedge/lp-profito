c946b4c890f622399f2d5133648ecf2fb3aebb3d
the-wedge <contas@thewedge.com.br>
2021-08-09 17:51:04 -0300

    2nd commit

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,6 @@
 export default function Button(props) {
     return(
-        <button type={props.type} class="bg-yellow-700 hover:bg-opacity-75 text-white py-2 px-6">
+        <button type={props.type} class="bg-yellow-700 hover:bg-opacity-75 text-white py-2 px-6 mt-3">
             {props.name}
         </button>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,72 @@
 import React from 'react';
 import Form from '../components/Form'
+import Button from '../components/Button'
 
 export default function Home() {
   return (
-    <div className="bg-cover bg-center bg-no-repeat" 
-         style={{ backgroundImage: `url("https://source.unsplash.com/random")` }}> 
-      <div className="flex flex-row items-center h-screen">
-        <div className="w-full md:w-3/5 ">
-          <h1 className="text-6xl text-white px-8 pt-8 pb-3 font-bold"> Investidor Profito</h1>
-          <h3 className="text-3xl text-white px-8 pt-3 pb-8"> Determinado, sonhador, exclusivo.</h3>
-        </div>
-        <div className="w-full md:w-2/5">
-          <Form />
+    <main>
+      <div id="banner" className="bg-cover bg-center bg-no-repeat" 
+          style={{ backgroundImage: `url("https://source.unsplash.com/random")` }}> 
+        <div className="flex flex-row items-center justify-center h-screen">
+          <div className="w-full lg:w-2/5 ">
+            <h1 className="text-6xl text-white px-8 pt-8 pb-3 font-bold"> Investidor Profitto</h1>
+            <h3 className="text-3xl text-white px-8 pt-3 pb-8"> Determinado, sonhador, exclusivo.</h3>
+          </div>
+          <div className="w-full lg:w-2/5">
+            <Form />
+          </div>
         </div>
       </div>
-    </div>
+      
+      <section id="sobre">
+        <div className="flex flex-row items-center justify-center py-8">
+          <div className=" flex w-max md:w-2/5 ">
+            <img className="w-5/12 rounded shadow-md" src="https://source.unsplash.com/random" />
+            <img className="w-5/12 rounded shadow-md m-8 h-auto" src="https://source.unsplash.com/random" />
+          </div>
+          <div className="w-max md:w-2/5">
+            <div className="container">
+              <h1 className="text-5xl"> What is Lorem Ipsum?</h1>
+              <p className="pt-5"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+              <a href="#banner">
+                <Button
+                name="Saiba mais"
+                type="button"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="ibge">
+        <div className="flex flex-row items-center justify-center py-8">
+          <div className=" flex w-full md:w-2/5 ">
+            <img className="w-5/12 rounded shadow-md m-2" src="https://source.unsplash.com/random" />
+            <img className="w-5/12 rounded shadow-md m-2" src="https://source.unsplash.com/random" />
+          </div>
+          <div className="w-full md:w-2/5">
+            <div className="container">
+              <h1 className="text-5xl"> What is Lorem Ipsum?</h1>
+              <p className="pt-5"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+              <a href="#banner">
+                <Button
+                name="Saiba mais"
+                type="button"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer className=" flex items-center bg-gray-500 text-white">
+        <img className="w-20 block m-auto rounded shadow-md mt-8 mb-3" src="https://source.unsplash.com/random" />
+
+      </footer>
 
 
+    </main>
+     
   )
 }
