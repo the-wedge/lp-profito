12d6d6ae1d71a4fb254c50bee8536719e166b58b
the-wedge <contas@thewedge.com.br>
2021-08-09 18:11:06 -0300

    3nd commit

--- components/Form.js
+++ components/Form.js
@@ -3,21 +3,21 @@ import Button from './Button'
 export default function Form() {
     return(
         <div className="w-full max-w-md">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <form className="bg-black bg-opacity-60 shadow-md rounded px-8 pt-8 pb-5 mb-4">
                 <div className="mb-3">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" for="nome">
+                    <label className="block text-white text-sm font-bold mb-2" for="nome">
                         Nome
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="nome" type="text" placeholder="Seu nome"/>
                 </div>
                 <div className="mb-3">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" for="email">
+                    <label className="block text-white text-sm font-bold mb-2" for="email">
                         Email
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="email@example.com"/>
                 </div>
-                <div className="mb-3">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" for="telefone">
+                <div>
+                    <label className="block text-white text-sm font-bold mb-2" for="telefone">
                         Telefone
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="(00) 0000-0000"/>
--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ export default function Home() {
     <main>
       <div id="banner" className="bg-cover bg-center bg-no-repeat" 
           style={{ backgroundImage: `url("https://source.unsplash.com/random")` }}> 
-        <div className="flex flex-row items-center justify-center h-screen">
-          <div className="w-full lg:w-2/5 ">
+        <div className="flex flex-wrap items-center justify-center h-screen">
+          <div className=" w-full lg:w-2/5 ">
             <h1 className="text-6xl text-white px-8 pt-8 pb-3 font-bold"> Investidor Profitto</h1>
             <h3 className="text-3xl text-white px-8 pt-3 pb-8"> Determinado, sonhador, exclusivo.</h3>
           </div>
@@ -19,7 +19,7 @@ export default function Home() {
       </div>
       
       <section id="sobre">
-        <div className="flex flex-row items-center justify-center py-8">
+        <div className="flex flex-wrap items-center justify-center py-14">
           <div className=" flex w-max md:w-2/5 ">
             <img className="w-5/12 rounded shadow-md" src="https://source.unsplash.com/random" />
             <img className="w-5/12 rounded shadow-md m-8 h-auto" src="https://source.unsplash.com/random" />
@@ -40,18 +40,16 @@ export default function Home() {
       </section>
 
       <section id="ibge">
-        <div className="flex flex-row items-center justify-center py-8">
-          <div className=" flex w-full md:w-2/5 ">
-            <img className="w-5/12 rounded shadow-md m-2" src="https://source.unsplash.com/random" />
-            <img className="w-5/12 rounded shadow-md m-2" src="https://source.unsplash.com/random" />
-          </div>
-          <div className="w-full md:w-2/5">
+        <div className="flex flex-row items-center justify-center py-14 text-white bg-black">
+          <div className="w-full md:w-3/5 text-center">
             <div className="container">
               <h1 className="text-5xl"> What is Lorem Ipsum?</h1>
+              <img className="w-20 block m-auto rounded shadow-md mt-8 mb-3" src="https://source.unsplash.com/random" />
+
               <p className="pt-5"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
               <a href="#banner">
                 <Button
-                name="Saiba mais"
+                name="Faça parte da equipe"
                 type="button"
                 />
               </a>
